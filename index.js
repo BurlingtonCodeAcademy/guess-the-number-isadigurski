6acd4d60589726d8ce4f71efc0d412a9d0730094
Ilya Sadigurski <isadigurski@gmail.com>
2020-02-09 21:26:51 -0500

    Final Version 4.0

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function randomInteger(min, max) {
   return min + Math.floor(Math.random() * range);
 }
 //...................................................................
-let max = 100
+let max =  100
 //...................................................................
 let min = 1
 //...................................................................
@@ -26,14 +26,18 @@ async function start() {
   console.log("\nLet's play a game where you (human) make up a number and I (computer) try to guess it.")
   let start = await ask('\n"Press" Enter To Start, Mwahaha!');
   let secretNumber = await ask('\nWhat is your secret number?' + "\n\nI won't peek, I promise..." + '\n\nEnter a number between 1-100 and "Press" Enter.\n\n');
-  console.log('\nYou entered: ' + secretNumber + '');
+  console.log('\nYou entered: ' + secretNumber);
   let guess = randomInteger(min, max)
   let firstGuess = await ask('\nIs it...' + guess + '?' + " Yes, or No: ")
 
+while (isNaN(firstGuess)) {
+  console.log('Invalid Input' + 'Please Type "Yes" or "No"')
+  return start()
+}
   if (firstGuess === "N" || firstGuess === "No" || firstGuess === "NO" || firstGuess === "no" || firstGuess === "n") {
     console.log("\nLet me try again")
-  } else {
-    console.log("\nYour number was " + secretNumber + "!" + '\n\n"No matter how dark the night, the morning always comes" - Lulu Final Fantasy X.\n\nI guessed  first try')
+  } if (firstGuess === "Y" || firstGuess === "Yes" || firstGuess === "YES" || firstGuess === "yes" || firstGuess === "y") {
+    console.log("\nYour number was " + secretNumber + "!" + '\n\n"No matter how dark the night, the morning always comes" - Lulu Final Fantasy X.\n\nI guessed first try')
     process.exit()
   }
 
@@ -41,12 +45,12 @@ async function start() {
 
     let secondGuess = await ask('\nIs it higher (H)?' + ',or lower (L)? ')
     if (secondGuess === "Higher" || secondGuess === "higher" || secondGuess === "High" || secondGuess === "high" || secondGuess === "H" || secondGuess === "h" || secondGuess === ">" || secondGuess === "+") {
-      min = guess
+      min = guess + 1
       guess = randomInteger(min, max)
       firstGuess = await ask('\nIs it...' + guess + '?' + " Yes, or No: ")
       guessCount += 1;
     } else if (secondGuess === "Lower" || secondGuess === "lower" || secondGuess === "Low" || secondGuess === "low" || secondGuess === "L" || secondGuess === "l" || secondGuess === "<" || secondGuess === "-") {
-      max = guess
+      max = guess - 1
       guess = randomInteger(min, max)
       firstGuess = await ask('\nIs it...' + guess + '?' + "Yes, or No: ")
       guessCount += 1;
@@ -58,19 +62,4 @@ async function start() {
       process.exit()
     }
   }
-
-
-  //Now try and complete the program
-
-  //Guess a random number, and ask if it's correct
-
-  //If the player says "yes" exit the game
-
-  //If the player says no
-  //Ask if it's higher or lower
-  //If it's higher modify min value
-  //If it's lower modify max value
-
-  //Guess a new number
-
 }
